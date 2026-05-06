9ef642a0462d8a39a4c160b5d3e5b09406db91d2
ShaswatRastogi <shaswatrastogi91@gmail.com>
2026-05-06 17:25:03 +0530

    Restore separate services architecture

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import psycopg2
-import os
 
-app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
+app = Flask(__name__)
 CORS(app)
 
 
@@ -236,14 +235,6 @@ def delete_task(id):
     return jsonify({
         "message": "Task Deleted Successfully"
     }), 200
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
